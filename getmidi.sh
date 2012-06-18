da2939de6c6750141ad010fb5845abafc5eb0cab
Sietse Brouwer <sbbrouwer@gmail.com>
2012-06-18 12:13:02 +0200

    Improve getmidi.sh
    * Add 'what is this for' comment
    * It can now be called with a specific prefix. Useful if you want to
      tell it which of multiple arrangments to create a files for.

--- getmidi.sh
+++ getmidi.sh
@@ -1,7 +1,10 @@
-# This function should be called from within the directory containing
-# the song's source files.
+# This helper function should be called from within the directory
+# containing the song's source files. It will save you some work by
+# creating the files song-layout-soprano.ly, song-layout-alto.ly, etc,
+# based on the template files `XXX-soprano.ly` etc. in the `templates`
+# directory.
 function getmidi {
-    thissong=`ls *-source.ly | sed 's_-source\.ly__'`
+    thissong=`ls $1*-source.ly | sed 's/-source\.ly//'`
     cp -v ../../template/XXX* ./
     sed -i "s/XXX/$thissong/" XXX*
     rename -v "s/XXX/$thissong/" XXX*
